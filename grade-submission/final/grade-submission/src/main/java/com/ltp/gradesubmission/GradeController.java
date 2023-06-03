d58c15b1a2a67d51d89017c72febc548fb2135fd
afnanzia97 <afnanzia97@yahoo.com>
2023-06-03 18:27:50 +0500

    grades mapping started

--- grade-submission/final/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/final/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -10,39 +10,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GradeController {
-
-    List<Grade> studentGrades = new ArrayList<>();
-
-    @GetMapping("/")
-    public String getForm(Model model, @RequestParam(required = false) String id) {
-        int index = getGradeIndex(id);
-        model.addAttribute("grade", index == Constants.NOT_FOUND ? new Grade() : studentGrades.get(index));
-        return "form";
-    }
-
-    @PostMapping("/handleSubmit")
-    public String submitForm(Grade grade) {
-        int index = getGradeIndex(grade.getId());
-        if (index == Constants.NOT_FOUND) {
-            studentGrades.add(grade);
-        } else {
-            studentGrades.set(index, grade);
-        }
-        return "redirect:/grades";
-    }
+public class GradeController 
+{
 
     @GetMapping("/grades")
-    public String getGrades(Model model) {
-        model.addAttribute("grades", studentGrades);
+    public String getGrades(Model model) 
+    {
+        Grade grade = new Grade("Harry", "Potions", "C+");
+        model.addAttribute("grade", grade);
         return "grades";
     }
-
-    public int getGradeIndex(String id) {
-        for (int i = 0; i < studentGrades.size(); i++) {
-            if (studentGrades.get(i).getId().equals(id)) return i;
-        }
-        return Constants.NOT_FOUND;
-    }
-
 }
+
+    
